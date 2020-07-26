d7a0e9d55006f0eec45e011263ce7a1d3735329a
Usybis <68807996+Usybis@users.noreply.github.com>
2020-07-26 11:48:06 +0200

    Delete bourse.js

--- bourse.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'bourse',
-    description: 'Test Command.',
-    execute(message) {
-        var result = Math.floor((Math.random() * 100) + 1)
-        message.channel.send("Voici le cour de la bourse en ce jour : " + result + " euros pour 1 MKNI");
-    }
-  }; 
